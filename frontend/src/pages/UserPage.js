8b9a1fa78ebd2bc3798264c2f04870eb740f2bcc
Girts Strazdins <girts.strazdins@gmail.com>
2023-03-22 18:25:37 +0100

    (WIP) Challenge listing page in progress

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,5 +1,20 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import "./UserPage.css";
+
+// TODO - load challenges from backend
+const challenges = [
+  { submitted: false },
+  { submitted: false },
+  { submitted: true },
+  { submitted: false },
+  { submitted: true },
+];
+
+function MenuIcon() {
+  return null;
+}
 
 /**
  * The main page for a regular non-admin user (team)
@@ -7,6 +22,23 @@ import { UserContext } from "../context/UserContext";
  * @constructor
  */
 export function UserPage() {
-  const userContext = useContext(UserContext);
-  return <h1>{userContext.user.username}</h1>;
+  const { user, setUser } = useContext(UserContext);
+
+  return (
+    <main>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">{user.username}</Typography>
+        </Toolbar>
+      </AppBar>
+      <h2>Choose a challenge</h2>
+      <div id="challenge-container">
+        {challenges.map((challenge, index) => (
+          <Button variant={challenge.submitted ? "contained" : "outlined"}>
+            {index + 1}
+          </Button>
+        ))}
+      </div>
+    </main>
+  );
 }
