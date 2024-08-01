7a535b9f9d1cc6b2f97ac5bdab2bc44de0372760
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-01 17:30:52 +0200

    Allow authorization in Swagger UI

---
+++ backend/src/main/java/no/strazdins/rebus/security/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package no.strazdins.rebus.security;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for OpenAPI.
+ * This allows to define endpoints requiring authentication.
+ */
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("API Documentation").version("1.0"))
+        .components(new Components()
+            .addSecuritySchemes("bearer-key",
+                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+        .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+  }
+}
