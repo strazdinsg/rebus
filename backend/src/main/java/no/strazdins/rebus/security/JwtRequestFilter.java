b4bc9bc1d02a18880e56b04d8f6ab92f320c09c8
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-09 17:48:45 +0200

    feat: allow to send JWT as a cookie

--- backend/src/main/java/no/strazdins/rebus/security/JwtRequestFilter.java
+++ backend/src/main/java/no/strazdins/rebus/security/JwtRequestFilter.java
@@ -2,9 +2,12 @@ package no.strazdins.rebus.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 import no.strazdins.rebus.services.UserService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -37,6 +40,15 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     try {
       if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
         jwt = authorizationHeader.substring(7);
+      } else {
+        // extract JWT from cookie
+        Optional<Cookie> jwtCookie = Arrays.stream(request.getCookies()).filter(c -> c.getName().equals("jwt")).findFirst();
+        if (jwtCookie.isPresent()) {
+          jwt = jwtCookie.get().getValue();
+        }
+      }
+
+      if (jwt != null) {
         userId = jwtUtil.extractUserId(jwt);
       }
 
