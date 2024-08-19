52ec865eac09bed3fc876b06450ecb6460b900ad
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-19 10:27:58 +0200

    feat: PoC Java unit tests

---
+++ backend/src/test/java/no/strazdins/rebus/ImageServiceTests.java
@@ -0,0 +1,20 @@
+package no.strazdins.rebus;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import no.strazdins.rebus.services.ImageService;
+import org.junit.jupiter.api.Test;
+
+class ImageServiceTests {
+  @Test
+  void testIsImage() {
+    assertFalse(ImageService.isImage(null));
+  }
+
+  @Test
+  void testIsImageContentType() {
+    assertFalse(ImageService.isImageContentType(null));
+    assertTrue(ImageService.isImageContentType("image/jpg"));
+  }
+}
