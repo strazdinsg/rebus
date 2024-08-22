05650ed854a7f691c0dda5eb3817d2ef0daab0a0
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-22 14:51:30 +0200

    chore: cleanup

--- frontend/src/Dummy.test.tsx
+++ frontend/src/Dummy.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, vi, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Dummy from "./Dummy";
