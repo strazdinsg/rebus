80f7ba7441ebd3a2f65451cd8ba3a72f50f78dcd
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-22 15:42:18 +0200

    refactor: create a generic test mock wrapper

--- frontend/src/pages/user/answer/AnswerPage.test.tsx
+++ frontend/src/pages/user/answer/AnswerPage.test.tsx
@@ -1,16 +1,13 @@
 import { screen, render } from "@testing-library/react";
 import { AnswerPage } from "./AnswerPage";
-import { BrowserRouter } from "react-router-dom";
 import {
   expectAppBarText,
   regularUser,
-  testQueryClient,
   waitForQueryToSettle,
+  TestMockWrapper,
 } from "../../../tests/testTools";
-import { UserContext } from "../../../context/UserContext";
 import { useChallenges } from "../../../queries/challengeQueries";
 import { mockChallenges } from "../../../tests/apiRequestMocks";
-import { QueryClientProvider } from "@tanstack/react-query";
 
 describe("AnswerPage tests", () => {
   it("Contains AppBar", async () => {
@@ -28,13 +25,9 @@ describe("AnswerPage tests", () => {
 
   async function renderAnswerPage() {
     render(
-      <BrowserRouter>
-        <QueryClientProvider client={testQueryClient}>
-          <UserContext.Provider value={{ user: regularUser, setUser: vi.fn() }}>
-            <AnswerPage challengeId={challengeId} />
-          </UserContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <TestMockWrapper user={regularUser} useRouter={true}>
+        <AnswerPage challengeId={challengeId} />,
+      </TestMockWrapper>
     );
 
     await waitForQueryToSettle(() => useChallenges());
--- frontend/src/tests/testTools.tsx
+++ frontend/src/tests/testTools.tsx
@@ -5,6 +5,8 @@ import {
   UseQueryResult,
 } from "@tanstack/react-query";
 import { ReactNode } from "react";
+import { UserContext, UserSession } from "../context/UserContext";
+import { BrowserRouter } from "react-router-dom";
 
 /**
  * Expects the app bar to have the given text
@@ -30,7 +32,7 @@ export function expectLoginPage() {
  */
 export async function waitForQueryToSettle(queryCreator: () => UseQueryResult) {
   const { result } = renderHook(queryCreator, {
-    wrapper: createWrapper(),
+    wrapper: queryClientWrapper,
   });
   await waitFor(() => expect(result.current.isSuccess).toBe(true));
 }
@@ -43,13 +45,43 @@ export const testQueryClient = new QueryClient({
   },
 });
 
-const createWrapper = () => {
-  return ({ children }: { children: ReactNode }) => (
+function queryClientWrapper({ children }: { children: ReactNode }) {
+  return (
     <QueryClientProvider client={testQueryClient}>
       {children}
     </QueryClientProvider>
   );
-};
+}
+
+/**
+ * Wraps the given children in several mock components needed for testing.
+ * @param children The child components to wrap
+ * @param user The user session to use
+ * @param useRouter When true, the children are wrapped in a BrowserRouter
+ */
+export function TestMockWrapper({
+  children,
+  user,
+  useRouter,
+}: {
+  children: ReactNode;
+  user: UserSession | null;
+  useRouter?: boolean;
+}) {
+  const content = (
+    <UserContext.Provider value={{ user: user, setUser: vi.fn() }}>
+      <QueryClientProvider client={testQueryClient}>
+        {children}
+      </QueryClientProvider>
+    </UserContext.Provider>
+  );
+
+  if (useRouter) {
+    return <BrowserRouter>{content}</BrowserRouter>;
+  } else {
+    return content;
+  }
+}
 
 /**
  * A regular user session.
