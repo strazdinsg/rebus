b47b6a2792a1d3e8987df0f6414f3542d2e61a0a
Girts Strazdins <girts.strazdins@gmail.com>
2024-08-23 15:10:15 +0200

    chore: cleanup

--- backend/src/main/java/no/strazdins/rebus/model/User.java
+++ backend/src/main/java/no/strazdins/rebus/model/User.java
@@ -5,9 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
