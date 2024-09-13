54c41938f4c78e316557599fd85cecc5f3b9aa66
Girts Strazdins <girts.strazdins@gmail.com>
2024-09-13 15:32:31 +0200

    tests: run tests on Firefox and Safari as well

--- e2e-testing/playwright.config.ts
+++ e2e-testing/playwright.config.ts
@@ -96,14 +96,14 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
